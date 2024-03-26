73cf81aa3f9f37b2b55bd727b654545094e34cd7
Piraveen2002 <146127350+Piraveen2002@users.noreply.github.com>
2024-03-26 14:32:06 -0400

    Add files via upload

---
+++ Wanted.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../App.css";
+
+function Wanted() {
+  return (
+    <div>
+      <div>
+        <h2>Items Wanted</h2>
+        <h3>Textbooks: </h3>
+        <br></br>
+        <h3>Supplies: </h3>
+        <br></br>
+        <h3>Clothing: </h3>
+        <br></br>
+      </div>
+    </div>
+  );
+}
+
+export default Wanted;
