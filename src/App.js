d7beb24383cb530684edb71b8fe19ae7f54e8de5
Piraveen2002 <146127350+Piraveen2002@users.noreply.github.com>
2024-03-26 21:09:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Wanted from "./components/Wanted";
